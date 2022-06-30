b5e3cbdad9fa631f2f828473a8b5e1aa2eed96c1
MIGUEL <mikiserrano2018@gmail.com>
2022-06-30 13:46:14 -0500

    tercer commit

--- conversor.py
+++ conversor.py
@@ -1,7 +1,38 @@
-pesos = input("¿cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar =3875
-dolares = pesos / valor_dolar 
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("tienes $" + dolares + " dolares ")
+menu = """"
+Bienvenido al conversor de modenas💕✌ 
+
+
+1 - pesos colombianos 
+2 - pesos argentinos
+3 - pesos mexicanos
+
+Elige una opcion:  """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+   pesos = input("¿cuantos pesos colombianos tienes?: ")
+   pesos = float(pesos)
+   valor_dolar =3875
+   dolares = pesos / valor_dolar 
+   dolares = round(dolares, 2)
+   dolares = str(dolares)
+   print("tienes $" + dolares + " dolares ")
+elif opcion == 2:
+     pesos = input("¿cuantos pesos argentinos tienes?: ")
+     pesos = float(pesos)
+     valor_dolar = 65
+     dolares = pesos / valor_dolar 
+     dolares = round(dolares, 2)
+     dolares = str(dolares)
+     print("tienes $" + dolares + " dolares ")
+elif opcion == 3:
+    pesos = input("¿cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar 
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares ")
+else:
+    print('Ingresa una opcion correcta por favor')
