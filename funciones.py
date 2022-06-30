14c69cf87c07872b6a5d03f2c8fe777f2605190e
MIGUEL <mikiserrano2018@gmail.com>
2022-06-30 14:09:07 -0500

    cuarto commit

---
+++ funciones.py
@@ -0,0 +1,27 @@
+def imprimir_mensaje():
+    print('mensaje especial: ')
+    print('!estoy aprendiendo a usar funciones! ')
+
+
+imprimir_mensaje()
+
+
+
+def conversacion(mensaje):
+    print('Hola')
+    print('Como estas')
+    print(mensaje)
+    print('Adios')
+
+
+opcion = int(input('elige una opcion(1, 2, 3): '))
+if opcion == 1:
+    conversacion('elegiste la opcion 1')
+elif opcion == 2:
+    conversacion('elegiste la opcion 2')
+elif opcion == 3:
+        conversacion('elegiste la opcion 3')
+else:
+    print('escribe la opciion correcta')
+
+    
