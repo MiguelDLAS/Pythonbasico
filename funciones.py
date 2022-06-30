2ac23e42bc6c32cfa0de5d429758589624f8a741
MIGUEL <mikiserrano2018@gmail.com>
2022-06-30 15:06:26 -0500

    quinto commit

--- funciones.py
+++ funciones.py
@@ -1,27 +1,34 @@
-def imprimir_mensaje():
-    print('mensaje especial: ')
-    print('!estoy aprendiendo a usar funciones! ')
+#def imprimir_mensaje():
+  #   print('mensaje especial: ')
+ #   print('!estoy aprendiendo a usar funciones! ')
 
 
-imprimir_mensaje()
+#imprimir_mensaje()
 
 
 
-def conversacion(mensaje):
-    print('Hola')
-    print('Como estas')
-    print(mensaje)
-    print('Adios')
+#def conversacion(mensaje):
+    #print('Hola')
+   # print('Como estas')
+  #  print(mensaje)
+ #   print('Adios')
 
 
-opcion = int(input('elige una opcion(1, 2, 3): '))
-if opcion == 1:
-    conversacion('elegiste la opcion 1')
-elif opcion == 2:
-    conversacion('elegiste la opcion 2')
-elif opcion == 3:
-        conversacion('elegiste la opcion 3')
-else:
-    print('escribe la opciion correcta')
+#opcion = int(input('elige una opcion(1, 2, 3): '))
+#if opcion == 1:
+   # conversacion('elegiste la opcion 1')
+#elif opcion == 2:
+ #   conversacion('elegiste la opcion 2')
+#elif opcion == 3:
+ #       conversacion('elegiste la opcion 3')
+#else:
+ #   print('escribe la opciion correcta')
 
-    
+
+def suma (a, b):
+    print('se suman dos numeros') 
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
